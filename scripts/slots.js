e4899d450297b1f8bea4e6d4778d74e224d66023
Anakin-Marc Zaeger <anizaeger@gmail.com>
2016-12-21 23:54:22 -0800

    Added comment to ease identifying reel stop positions.

--- scripts/slots.js
+++ scripts/slots.js
@@ -63,6 +63,9 @@ var virtStop = new Array(numReels);
 var strip = new Array(numReels);
 var numVirtStops = new Array(numReels);
 
+// Reel Stop		   0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 2 2
+// Numbers		   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+
 strip[0] 		= [0,4,0,7,0,3,0,5,0,2,0,4,0,1,0,5,0,3,0,6,0,2];
 numVirtStops[0]		= [4,1,5,1,6,3,4,1,3,2,5,1,3,6,4,1,5,3,5,1,5,3];
 
